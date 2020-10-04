dcb79111f5cb0c6a7af1d5f09a17085ae3302706
Vladimir Zelenokor <keksikoz.hs@yandex.ru>
2020-10-04 16:02:18 +0300

    added all

---
+++ Week 5/ex 5/ex5.v
@@ -0,0 +1,24 @@
+module ex5	
+(
+input clk ,
+input d,
+output q,
+output q_n
+);
+
+wire n1;
+
+ex4 master
+(
+	. clk ( ~ clk ) ,
+	.d ( d ) ,
+		.q ( n1 )
+);
+ex4 slave 
+(
+	. clk ( clk ) ,
+	.d ( n1 ) ,
+	.q ( q ) ,
+	.q_n ( q_n )
+);
+endmodule 
